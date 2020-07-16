d49cd38f17d2e9e32f1a27d09d8dffd0b7ca5b31
taewoong <nutopia002@naver.com>
2020-07-16 11:27:57 +0900

    t
    commit
    ;
    commit;

---
+++ test0702.sql
@@ -0,0 +1,17 @@
+insert into test
+values(2, 'soobin','F','haha',24,'moon');
+
+insert into test
+values(3, 'danbi','F','haha',26,'Fri');
+
+insert into test
+values(4, 'sungjin','M','haha',29,'Mon');
+
+insert into test
+values(5, 'Heymin','F','haha',29,'Tues');
+
+insert into test
+values(6, 'Yechan','M','haha',29,'Wed');
+
+insert into test
+values(7, 'Yong','M','oooo',100,'Thur');
